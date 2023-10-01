2d9c49843bfa165e867c01bf012a6adaf5c21ed6
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-01 14:15:09 +0330

    remove footer

--- src/@core/layouts/components/shared-components/footer/FooterContent.js
+++ src/@core/layouts/components/shared-components/footer/FooterContent.js
@@ -5,12 +5,12 @@ import Typography from '@mui/material/Typography'
 import useMediaQuery from '@mui/material/useMediaQuery'
 
 const FooterContent = () => {
-  // ** Var
-  const hidden = useMediaQuery(theme => theme.breakpoints.down('md'))
+    // ** Var
+    const hidden = useMediaQuery(theme => theme.breakpoints.down('md'))
 
-  return (
-    <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
-      <Typography sx={{ mr: 2 }}>
+    return (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap', alignItems: 'center', justifyContent: 'space-between' }}>
+            {/* <Typography sx={{ mr: 2 }}>
         {`© ${new Date().getFullYear()}, Made with `}
         <Box component='span' sx={{ color: 'error.main' }}>
           ❤️
@@ -38,9 +38,9 @@ const FooterContent = () => {
             Support
           </Link>
         </Box>
-      )}
-    </Box>
-  )
+      )} */}
+        </Box>
+    )
 }
 
 export default FooterContent
