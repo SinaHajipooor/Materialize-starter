a5db3d4ad18cdbaba86320c87f44834ff1abf1c0
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-02 14:05:28 +0330

    style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -15,45 +15,45 @@ import FooterIllustrations from 'src/views/pages/misc/FooterIllustrations'
 
 // ** Styled Components
 const BoxWrapper = styled(Box)(({ theme }) => ({
-  [theme.breakpoints.down('md')]: {
-    width: '90vw'
-  }
+    [theme.breakpoints.down('md')]: {
+        width: '90vw'
+    }
 }))
 
 const Img = styled('img')(({ theme }) => ({
-  marginTop: theme.spacing(15),
-  marginBottom: theme.spacing(15),
-  [theme.breakpoints.down('lg')]: {
-    height: 450,
-    marginTop: theme.spacing(10),
-    marginBottom: theme.spacing(10)
-  },
-  [theme.breakpoints.down('md')]: {
-    height: 400
-  }
+    marginTop: theme.spacing(15),
+    marginBottom: theme.spacing(15),
+    [theme.breakpoints.down('lg')]: {
+        height: 450,
+        marginTop: theme.spacing(10),
+        marginBottom: theme.spacing(10)
+    },
+    [theme.breakpoints.down('md')]: {
+        height: 400
+    }
 }))
 
 const Error404 = () => {
-  return (
-    <Box className='content-center'>
-      <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
-        <BoxWrapper>
-          <Typography variant='h1' sx={{ mb: 2.5 }}>
-            404
-          </Typography>
-          <Typography variant='h5' sx={{ mb: 2.5, letterSpacing: '0.18px', fontSize: '1.5rem !important' }}>
-            Page Not Found ⚠️
-          </Typography>
-          <Typography variant='body2'>We couldn&prime;t find the page you are looking for.</Typography>
-        </BoxWrapper>
-        <Img alt='error-illustration' src='/images/pages/404.png' />
-        <Button href='/' component={Link} variant='contained' sx={{ px: 5.5 }}>
-          Back to Home
-        </Button>
-      </Box>
-      <FooterIllustrations image='/images/pages/misc-404-object.png' />
-    </Box>
-  )
+    return (
+        <Box className='content-center'>
+            <Box sx={{ p: 5, display: 'flex', flexDirection: 'column', alignItems: 'center', textAlign: 'center' }}>
+                <BoxWrapper>
+                    <Typography variant='h1' sx={{ mb: 2.5 }}>
+                        404
+                    </Typography>
+                    <Typography variant='h5' sx={{ mb: 2.5, letterSpacing: '0.18px', fontSize: '1.5rem !important' }}>
+                        صفحه مورد نظر یافت نشد
+                    </Typography>
+                    <Typography variant='body2'>ما نتوانستیم صفحه مورد نظر شما را پیدا کنیم</Typography>
+                </BoxWrapper>
+                <Img alt='error-illustration' src='/images/pages/404.png' />
+                <Button href='/' component={Link} variant='contained' sx={{ px: 5.5 }}>
+                    Back to Home
+                </Button>
+            </Box>
+            <FooterIllustrations image='/images/pages/misc-404-object.png' />
+        </Box>
+    )
 }
 Error404.getLayout = page => <BlankLayout>{page}</BlankLayout>
 
