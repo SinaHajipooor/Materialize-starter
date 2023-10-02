1b866c27e277907001eda97d21c80e409b0b3484
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-02 12:57:28 +0330

    logout user

--- src/@core/layouts/components/shared-components/UserDropdown.js
+++ src/@core/layouts/components/shared-components/UserDropdown.js
@@ -19,157 +19,155 @@ import Icon from 'src/@core/components/icon'
 
 // ** Context
 import { useAuth } from 'src/hooks/useAuth'
+import { signOut } from 'next-auth/react'
 
 // ** Styled Components
 const BadgeContentSpan = styled('span')(({ theme }) => ({
-  width: 8,
-  height: 8,
-  borderRadius: '50%',
-  backgroundColor: theme.palette.success.main,
-  boxShadow: `0 0 0 2px ${theme.palette.background.paper}`
+    width: 8,
+    height: 8,
+    borderRadius: '50%',
+    backgroundColor: theme.palette.success.main,
+    boxShadow: `0 0 0 2px ${theme.palette.background.paper}`
 }))
 
 const UserDropdown = props => {
-  // ** Props
-  const { settings } = props
+    // ** Props
+    const { settings } = props
 
-  // ** States
-  const [anchorEl, setAnchorEl] = useState(null)
+    // ** States
+    const [anchorEl, setAnchorEl] = useState(null)
 
-  // ** Hooks
-  const router = useRouter()
-  const { logout } = useAuth()
+    // ** Hooks
+    const router = useRouter()
+    const { logout } = useAuth()
 
-  // ** Vars
-  const { direction } = settings
+    // ** Vars
+    const { direction } = settings
 
-  const handleDropdownOpen = event => {
-    setAnchorEl(event.currentTarget)
-  }
-
-  const handleDropdownClose = url => {
-    if (url) {
-      router.push(url)
+    const handleDropdownOpen = event => {
+        setAnchorEl(event.currentTarget)
     }
-    setAnchorEl(null)
-  }
 
-  const styles = {
-    py: 2,
-    px: 4,
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
-    color: 'text.primary',
-    textDecoration: 'none',
-    '& svg': {
-      mr: 2,
-      fontSize: '1.375rem',
-      color: 'text.primary'
+    const handleDropdownClose = url => {
+        if (url) {
+            router.push(url)
+        }
+        setAnchorEl(null)
     }
-  }
 
-  const handleLogout = () => {
-    logout()
-    handleDropdownClose()
-  }
+    const styles = {
+        py: 2,
+        px: 4,
+        width: '100%',
+        display: 'flex',
+        alignItems: 'center',
+        color: 'text.primary',
+        textDecoration: 'none',
+        '& svg': {
+            mr: 2,
+            fontSize: '1.375rem',
+            color: 'text.primary'
+        }
+    }
 
-  return (
-    <Fragment>
-      <Badge
-        overlap='circular'
-        onClick={handleDropdownOpen}
-        sx={{ ml: 2, cursor: 'pointer' }}
-        badgeContent={<BadgeContentSpan />}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right'
-        }}
-      >
-        <Avatar
-          alt='John Doe'
-          onClick={handleDropdownOpen}
-          sx={{ width: 40, height: 40 }}
-          src='/images/avatars/1.png'
-        />
-      </Badge>
-      <Menu
-        anchorEl={anchorEl}
-        open={Boolean(anchorEl)}
-        onClose={() => handleDropdownClose()}
-        sx={{ '& .MuiMenu-paper': { width: 230, mt: 4 } }}
-        anchorOrigin={{ vertical: 'bottom', horizontal: direction === 'ltr' ? 'right' : 'left' }}
-        transformOrigin={{ vertical: 'top', horizontal: direction === 'ltr' ? 'right' : 'left' }}
-      >
-        <Box sx={{ pt: 2, pb: 3, px: 4 }}>
-          <Box sx={{ display: 'flex', alignItems: 'center' }}>
+
+
+    return (
+        <Fragment>
             <Badge
-              overlap='circular'
-              badgeContent={<BadgeContentSpan />}
-              anchorOrigin={{
-                vertical: 'bottom',
-                horizontal: 'right'
-              }}
+                overlap='circular'
+                onClick={handleDropdownOpen}
+                sx={{ ml: 2, cursor: 'pointer' }}
+                badgeContent={<BadgeContentSpan />}
+                anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right'
+                }}
             >
-              <Avatar alt='John Doe' src='/images/avatars/1.png' sx={{ width: '2.5rem', height: '2.5rem' }} />
+                <Avatar
+                    alt='John Doe'
+                    onClick={handleDropdownOpen}
+                    sx={{ width: 40, height: 40 }}
+                    src='/images/avatars/1.png'
+                />
             </Badge>
-            <Box sx={{ display: 'flex', ml: 3, alignItems: 'flex-start', flexDirection: 'column' }}>
-              <Typography sx={{ fontWeight: 600 }}>John Doe</Typography>
-              <Typography variant='body2' sx={{ fontSize: '0.8rem', color: 'text.disabled' }}>
-                Admin
-              </Typography>
-            </Box>
-          </Box>
-        </Box>
-        <Divider sx={{ mt: '0 !important' }} />
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:account-outline' />
-            Profile
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:email-outline' />
-            Inbox
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:message-outline' />
-            Chat
-          </Box>
-        </MenuItem>
-        <Divider />
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:cog-outline' />
-            Settings
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:currency-usd' />
-            Pricing
-          </Box>
-        </MenuItem>
-        <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
-          <Box sx={styles}>
-            <Icon icon='mdi:help-circle-outline' />
-            FAQ
-          </Box>
-        </MenuItem>
-        <Divider />
-        <MenuItem
-          onClick={handleLogout}
-          sx={{ py: 2, '& svg': { mr: 2, fontSize: '1.375rem', color: 'text.primary' } }}
-        >
-          <Icon icon='mdi:logout-variant' />
-          Logout
-        </MenuItem>
-      </Menu>
-    </Fragment>
-  )
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={() => handleDropdownClose()}
+                sx={{ '& .MuiMenu-paper': { width: 230, mt: 4 } }}
+                anchorOrigin={{ vertical: 'bottom', horizontal: direction === 'ltr' ? 'right' : 'left' }}
+                transformOrigin={{ vertical: 'top', horizontal: direction === 'ltr' ? 'right' : 'left' }}
+            >
+                <Box sx={{ pt: 2, pb: 3, px: 4 }}>
+                    <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                        <Badge
+                            overlap='circular'
+                            badgeContent={<BadgeContentSpan />}
+                            anchorOrigin={{
+                                vertical: 'bottom',
+                                horizontal: 'right'
+                            }}
+                        >
+                            <Avatar alt='John Doe' src='/images/avatars/1.png' sx={{ width: '2.5rem', height: '2.5rem' }} />
+                        </Badge>
+                        <Box sx={{ display: 'flex', ml: 3, alignItems: 'flex-start', flexDirection: 'column' }}>
+                            <Typography sx={{ fontWeight: 600 }}>John Doe</Typography>
+                            <Typography variant='body2' sx={{ fontSize: '0.8rem', color: 'text.disabled' }}>
+                                Admin
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Box>
+                <Divider sx={{ mt: '0 !important' }} />
+                <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
+                    <Box sx={styles}>
+                        <Icon icon='mdi:account-outline' />
+                        Profile
+                    </Box>
+                </MenuItem>
+                <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
+                    <Box sx={styles}>
+                        <Icon icon='mdi:email-outline' />
+                        Inbox
+                    </Box>
+                </MenuItem>
+                <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
+                    <Box sx={styles}>
+                        <Icon icon='mdi:message-outline' />
+                        Chat
+                    </Box>
+                </MenuItem>
+                <Divider />
+                <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
+                    <Box sx={styles}>
+                        <Icon icon='mdi:cog-outline' />
+                        Settings
+                    </Box>
+                </MenuItem>
+                <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
+                    <Box sx={styles}>
+                        <Icon icon='mdi:currency-usd' />
+                        Pricing
+                    </Box>
+                </MenuItem>
+                <MenuItem sx={{ p: 0 }} onClick={() => handleDropdownClose()}>
+                    <Box sx={styles}>
+                        <Icon icon='mdi:help-circle-outline' />
+                        FAQ
+                    </Box>
+                </MenuItem>
+                <Divider />
+                <MenuItem
+                    onClick={() => signOut()}
+                    sx={{ py: 2, '& svg': { mr: 2, fontSize: '1.375rem', color: 'text.primary' } }}
+                >
+                    <Icon icon='mdi:logout-variant' />
+                    Logout
+                </MenuItem>
+            </Menu>
+        </Fragment>
+    )
 }
 
 export default UserDropdown
