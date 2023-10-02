003c29a8a5f0889d512b4426acde3169f55f01d8
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-02 14:44:29 +0330

    add animation for home page

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,42 +1,121 @@
 // ** MUI Imports
-import Card from '@mui/material/Card'
 import Grid from '@mui/material/Grid'
-import Typography from '@mui/material/Typography'
-import CardHeader from '@mui/material/CardHeader'
-import CardContent from '@mui/material/CardContent'
+
+// ** Icon Imports
+import Icon from 'src/@core/components/icon'
+
+// ** Custom Component Import
+import CardStatisticsVertical from 'src/@core/components/card-statistics/card-stats-vertical'
+
+// ** Styled Component Import
+import ApexChartWrapper from 'src/@core/styles/libs/react-apexcharts'
+
+// ** Demo Components Imports
+import CrmAward from 'src/views/dashboards/crm/CrmAward'
+import CrmTable from 'src/views/dashboards/crm/CrmTable'
+import CrmTotalGrowth from 'src/views/dashboards/crm/CrmTotalGrowth'
+import CrmTotalProfit from 'src/views/dashboards/crm/CrmTotalProfit'
+import CrmMonthlyBudget from 'src/views/dashboards/crm/CrmMonthlyBudget'
+import CrmExternalLinks from 'src/views/dashboards/crm/CrmExternalLinks'
+import CrmWeeklyOverview from 'src/views/dashboards/crm/CrmWeeklyOverview'
+import CrmPaymentHistory from 'src/views/dashboards/crm/CrmPaymentHistory'
+import CrmOrganicSessions from 'src/views/dashboards/crm/CrmOrganicSessions'
+import CrmProjectTimeline from 'src/views/dashboards/crm/CrmProjectTimeline'
+import CrmMeetingSchedule from 'src/views/dashboards/crm/CrmMeetingSchedule'
+import CrmSocialNetworkVisits from 'src/views/dashboards/crm/CrmSocialNetworkVisits'
+import CrmMostSalesInCountries from 'src/views/dashboards/crm/CrmMostSalesInCountries'
 import { Grow } from '@mui/material'
 
 const Home = () => {
     return (
-        <Grid container spacing={6}>
-            <Grid item xs={12}>
+        <ApexChartWrapper>
+            <Grid container spacing={7} className='match-height'>
+                <Grow in timeout={1 * 500}>
+                    <Grid item xs={12} sm={3} md={2}>
+                        <CardStatisticsVertical
+                            stats='155k'
+                            color='primary'
+                            trendNumber='+22%'
+                            title='Total Orders'
+                            chipText='Last 4 Month'
+                            icon={<Icon icon='mdi:cart-plus' />}
+                        />
+                    </Grid>
+                </Grow>
+                <Grow in timeout={1.5 * 500}>
+                    <Grid item xs={12} sm={3} md={2}>
+                        <CardStatisticsVertical
+                            stats='$13.4k'
+                            color='success'
+                            trendNumber='+38%'
+                            title='Total Sales'
+                            chipText='Last Six Month'
+                            icon={<Icon icon='mdi:currency-usd' />}
+                        />
+                    </Grid>
+                </Grow>
                 <Grow in timeout={2 * 500}>
-                    <Card>
-                        <CardHeader title='Kick start your project 🚀'></CardHeader>
-                        <CardContent>
-                            <Typography sx={{ mb: 2 }}>All the best for your new project.</Typography>
-                            <Typography>
-                                Please make sure to read our Template Documentation to understand where to go from here and how to use our
-                                template.
-                            </Typography>
-                        </CardContent>
-                    </Card>
+                    <Grid item xs={12} sm={3} md={2}>
+                        <CrmTotalProfit />
+                    </Grid>
                 </Grow>
-            </Grid>
-            <Grid item xs={12}>
+                <Grow in timeout={2.5 * 500}>
+                    <Grid item xs={12} sm={3} md={2}>
+                        <CrmTotalGrowth />
+                    </Grid>
+                </Grow>
                 <Grow in timeout={3 * 500}>
-                    <Card>
-                        <CardHeader title='ACL and JWT 🔒'></CardHeader>
-                        <CardContent>
-                            <Typography sx={{ mb: 2 }}>
-                                Access Control (ACL) and Authentication (JWT) are the two main security features of our template and are implemented in the starter-kit as well.
-                            </Typography>
-                            <Typography>Please read our Authentication and ACL Documentations to get more out of them.</Typography>
-                        </CardContent>
-                    </Card>
+                    <Grid item xs={12} sm={3} md={2}>
+                        <CrmTotalGrowth />
+                    </Grid>
+                </Grow>
+                <Grow in timeout={3.5 * 500}>
+                    <Grid item xs={12} sm={3} md={2}>
+                        <CrmTotalProfit />
+                    </Grid>
+                </Grow>
+                {/* <Grid item xs={12} md={4}>
+                    <CrmOrganicSessions />
+                </Grid> */}
+                {/* <Grid item xs={12} md={8}>
+                    <CrmProjectTimeline />
+                </Grid> */}
+                {/* <Grid item xs={12} sm={6} md={4}>
+                    <CrmWeeklyOverview />
+                </Grid> */}
+                <Grow in timeout={2.5 * 500}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <CrmSocialNetworkVisits />
+                    </Grid>
+                </Grow>
+                <Grow in timeout={3 * 500}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <CrmMonthlyBudget />
+                    </Grid>
+                </Grow>
+                <Grow in timeout={4 * 500}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <CrmMeetingSchedule />
+                    </Grid>
+                </Grow>
+                <Grow in timeout={6 * 500}>
+                    <Grid item xs={12} sm={6} md={4}>
+                        <CrmExternalLinks />
+                    </Grid>
+                </Grow>
+                {/* <Grid item xs={12} sm={6} md={4}>
+                    <CrmPaymentHistory />
+                </Grid>
+                <Grid item xs={12} md={4}>
+                    <CrmMostSalesInCountries />
+                </Grid> */}
+                <Grow in timeout={6.5 * 500}>
+                    <Grid item xs={12} md={8}>
+                        <CrmTable />
+                    </Grid>
                 </Grow>
             </Grid>
-        </Grid>
+        </ApexChartWrapper >
     )
 }
 
