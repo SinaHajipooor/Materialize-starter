6eb7049ca459d379c99dca56cf962bebf5d9e771
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-02 19:17:53 +0330

    add axios config

---
+++ src/utils/axios.js
@@ -0,0 +1,10 @@
+import axios from 'axios';
+
+const axiosConfig = axios.create({
+    baseURL: 'http://45.149.77.156:8081',  // BASE URL
+    headers: {
+        'Content-Type': 'application/json', // CONTENT TYPE
+    },
+});
+
+export default axiosConfig;
