b0509e7f67bfd54ebd1f207e59fc8fb0c8a1c32d
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-02 20:01:26 +0330

    index query

--- src/pages/activityHistories/hooks/useActivityIndex.js
+++ src/pages/activityHistories/hooks/useActivityIndex.js
@@ -1,4 +1,12 @@
+import { useQuery } from "@tanstack/react-query";
+import { apiFetchAllActivityHistories } from "src/api/activityHistories/apiActivityHistories";
+
 // get activity histories index
 export default function useActivityIndex() {
+    const { isLoading, data: activityHistories } = useQuery({
+        queryKey: ['activity-histories'],
+        queryFn: apiFetchAllActivityHistories,
+    });
 
+    return { isLoading, activityHistories }
 }
