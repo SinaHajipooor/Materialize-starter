423411539e6bf22fb26acb2aa842611ca15e5daf
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-05 09:56:45 +0330

    chnage calendar locale

--- src/store/apps/calendar/index.js
+++ src/store/apps/calendar/index.js
@@ -6,85 +6,85 @@ import axios from 'axios'
 
 // ** Fetch Events
 export const fetchEvents = createAsyncThunk('appCalendar/fetchEvents', async calendars => {
-  const response = await axios.get('/apps/calendar/events', {
-    params: {
-      calendars
-    }
-  })
+    const response = await axios.get('/apps/calendar/events', {
+        params: {
+            calendars
+        }
+    })
 
-  return response.data
+    return response.data
 })
 
 // ** Add Event
 export const addEvent = createAsyncThunk('appCalendar/addEvent', async (event, { dispatch }) => {
-  const response = await axios.post('/apps/calendar/add-event', {
-    data: {
-      event
-    }
-  })
-  await dispatch(fetchEvents(['Personal', 'Business', 'Family', 'Holiday', 'ETC']))
+    const response = await axios.post('/apps/calendar/add-event', {
+        data: {
+            event
+        }
+    })
+    await dispatch(fetchEvents(['Personal', 'Business', 'Family', 'Holiday', 'ETC']))
 
-  return response.data.event
+    return response.data.event
 })
 
 // ** Update Event
 export const updateEvent = createAsyncThunk('appCalendar/updateEvent', async (event, { dispatch }) => {
-  const response = await axios.post('/apps/calendar/update-event', {
-    data: {
-      event
-    }
-  })
-  await dispatch(fetchEvents(['Personal', 'Business', 'Family', 'Holiday', 'ETC']))
+    const response = await axios.post('/apps/calendar/update-event', {
+        data: {
+            event
+        }
+    })
+    await dispatch(fetchEvents(['Personal', 'Business', 'Family', 'Holiday', 'ETC']))
 
-  return response.data.event
+    return response.data.event
 })
 
 // ** Delete Event
 export const deleteEvent = createAsyncThunk('appCalendar/deleteEvent', async (id, { dispatch }) => {
-  const response = await axios.delete('/apps/calendar/remove-event', {
-    params: { id }
-  })
-  await dispatch(fetchEvents(['Personal', 'Business', 'Family', 'Holiday', 'ETC']))
+    const response = await axios.delete('/apps/calendar/remove-event', {
+        params: { id }
+    })
+    await dispatch(fetchEvents(['Personal', 'Business', 'Family', 'Holiday', 'ETC']))
 
-  return response.data
+    return response.data
 })
 
 export const appCalendarSlice = createSlice({
-  name: 'appCalendar',
-  initialState: {
-    events: [],
-    selectedEvent: null,
-    selectedCalendars: ['Personal', 'Business', 'Family', 'Holiday', 'ETC']
-  },
-  reducers: {
-    handleSelectEvent: (state, action) => {
-      state.selectedEvent = action.payload
+    name: 'appCalendar',
+    initialState: {
+        events: [],
+        selectedEvent: null,
+        selectedCalendars: ['Personal', 'Business', 'Family', 'Holiday', 'ETC']
     },
-    handleCalendarsUpdate: (state, action) => {
-      const filterIndex = state.selectedCalendars.findIndex(i => i === action.payload)
-      if (state.selectedCalendars.includes(action.payload)) {
-        state.selectedCalendars.splice(filterIndex, 1)
-      } else {
-        state.selectedCalendars.push(action.payload)
-      }
-      if (state.selectedCalendars.length === 0) {
-        state.events.length = 0
-      }
+    reducers: {
+        handleSelectEvent: (state, action) => {
+            state.selectedEvent = action.payload
+        },
+        handleCalendarsUpdate: (state, action) => {
+            const filterIndex = state.selectedCalendars.findIndex(i => i === action.payload)
+            if (state.selectedCalendars.includes(action.payload)) {
+                state.selectedCalendars.splice(filterIndex, 1)
+            } else {
+                state.selectedCalendars.push(action.payload)
+            }
+            if (state.selectedCalendars.length === 0) {
+                state.events.length = 0
+            }
+        },
+        handleAllCalendars: (state, action) => {
+            const value = action.payload
+            if (value === true) {
+                state.selectedCalendars = ['Personal', 'Business', 'Family', 'Holiday', 'ETC']
+            } else {
+                state.selectedCalendars = []
+            }
+        }
     },
-    handleAllCalendars: (state, action) => {
-      const value = action.payload
-      if (value === true) {
-        state.selectedCalendars = ['Personal', 'Business', 'Family', 'Holiday', 'ETC']
-      } else {
-        state.selectedCalendars = []
-      }
+    extraReducers: builder => {
+        builder.addCase(fetchEvents.fulfilled, (state, action) => {
+            state.events = action.payload
+        })
     }
-  },
-  extraReducers: builder => {
-    builder.addCase(fetchEvents.fulfilled, (state, action) => {
-      state.events = action.payload
-    })
-  }
 })
 
 export const { handleSelectEvent, handleCalendarsUpdate, handleAllCalendars } = appCalendarSlice.actions
