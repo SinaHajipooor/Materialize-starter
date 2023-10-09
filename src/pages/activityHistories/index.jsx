7496873f781a5b8920108e64f84f3d8c4f13debf
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-09 17:29:45 +0330

    optimize activity index

--- src/pages/activityHistories/index.jsx
+++ src/pages/activityHistories/index.jsx
@@ -1,21 +1,9 @@
 // ** MUI Imports
 import Grid from '@mui/material/Grid'
-import Link from '@mui/material/Link'
-import Typography from '@mui/material/Typography'
-
-// ** Custom Components Imports
-import PageHeader from 'src/@core/components/page-header'
 
 // ** Demo Components Imports
-import TableBasic from 'src/views/table/data-grid/TableBasic'
-import TableFilter from 'src/views/table/data-grid/TableFilter'
-import TableColumns from 'src/views/table/data-grid/TableColumns'
-import TableEditable from 'src/views/table/data-grid/TableEditable'
-import TableBasicSort from 'src/views/table/data-grid/TableBasicSort'
-import TableSelection from 'src/views/table/data-grid/TableSelection'
 import TableServerSide from 'src/views/table/data-grid/TableServerSide'
 import useActivityIndex from './hooks/useActivityIndex'
-import { CircularProgress } from '@mui/material'
 import Spinner from 'src/components/Spinner'
 
 const DataGrid = () => {
