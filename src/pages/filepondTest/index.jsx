3dfe3dff8c27e1b08416c2dd4b1db55173637dda
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-09 19:23:18 +0330

    add file pond

---
+++ src/pages/filepondTest/index.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+// Import React FilePond
+import { FilePond, File, registerPlugin } from 'react-filepond'
+
+// Import FilePond styles
+import 'filepond/dist/filepond.min.css'
+
+// Import the Image EXIF Orientation and Image Preview plugins
+// Note: These need to be installed separately
+// `npm i filepond-plugin-image-preview filepond-plugin-image-exif-orientation --save`
+import FilePondPluginImageExifOrientation from 'filepond-plugin-image-exif-orientation'
+import FilePondPluginImagePreview from 'filepond-plugin-image-preview'
+import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css'
+
+// Register the plugins
+registerPlugin(FilePondPluginImageExifOrientation, FilePondPluginImagePreview)
+
+// Our app
+export default function App() {
+    const [files, setFiles] = useState([])
+
+    return (
+
+        <FilePond
+
+            imagePreviewHeight={200}
+            files={files}
+            onupdatefiles={setFiles}
+            allowMultiple={true}
+            maxFiles={20}
+            server="/api"
+            name="files"
+            labelIdle='فایل های مورد نظر خود را انتخاب کنید'
+            labelFileProcessingError='خطایی رخ داد'
+            labelTapToRetry='دوباره تلاش کن'
+            styleItemPanelAspectRatio='0.25'
+        />
+    )
+}
