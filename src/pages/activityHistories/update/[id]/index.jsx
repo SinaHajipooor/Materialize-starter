bdb33c7dd6a39d94a7fe1a5e21fec2f7e04a7b36
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-12 10:07:13 +0330

    make update page static generation

--- src/pages/activityHistories/update/[id]/index.jsx
+++ src/pages/activityHistories/update/[id]/index.jsx
@@ -6,17 +6,42 @@ import { useRouter } from 'next/router'
 import useActivityDetails from '../../hooks/useActivityDetails'
 import Spinner from 'src/components/Spinner'
 import UpdateActivityHistory from '../components/UpdateActivityHistory'
+import { apiShowActivityHistory } from 'src/api/activityHistories/apiActivityHistories'
 
-const UpdateForm = () => {
-    const router = useRouter()
-    const id = router.query.id
-    const { isLoading, activityHistory } = useActivityDetails(id)
+const UpdateForm = ({ activityHistory }) => {
+    //     const router = useRouter()
+    //     const id = router.query.id
+    //     const { isLoading, activityHistory } = useActivityDetails(id)
+
+    //     return (
+    //         <Grid item xs={12}>
+    //             {isLoading ? <Spinner /> : <UpdateActivityHistory activityHistory={activityHistory} />}
+    //         </Grid>
+    //     )
+
 
     return (
         <Grid item xs={12}>
-            {isLoading ? <Spinner /> : <UpdateActivityHistory activityHistory={activityHistory} />}
+            <UpdateActivityHistory activityHistory={activityHistory} />
         </Grid>
     )
+
 }
 
 export default UpdateForm
+
+
+export async function getServerSideProps(context) {
+    // extract activity id
+    const { params } = context;
+    const activityId = params.id;
+
+    // get activity details 
+    const data = await apiShowActivityHistory(activityId);
+
+    return {
+        props: {
+            activityHistory: data
+        }
+    }
+}
