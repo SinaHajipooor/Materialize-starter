9bcda6c7ee448927db1f385c708bd20744ff0354
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-12 14:08:35 +0330

    combinate SSG with react-query

--- src/pages/activityHistories/index.jsx
+++ src/pages/activityHistories/index.jsx
@@ -7,32 +7,26 @@ import TableServerSide from 'src/views/table/data-grid/TableServerSide'
 // import useActivityIndex from './hooks/useActivityIndex'
 import Spinner from 'src/components/Spinner'
 import { apiFetchAllActivityHistories } from 'src/api/activityHistories/apiActivityHistories'
+import { useQuery } from '@tanstack/react-query'
+import useActivityIndex from 'src/hooks/activity-history/useActivityIndex'
+
 
 const DataGrid = ({ activityHistories }) => {
 
-    //     const { isLoading, activityHistories } = useActivityIndex()
+    const { data } = useActivityIndex(activityHistories)
 
-    //     return (
-    //         isLoading ? <Spinner /> : <Grid container spacing={6}>
-    //             <Grid item xs={12}>
-    //                 <TableServerSide activityHistories={activityHistories} />
-    //             </Grid>
-    //         </Grid>
-    //     )
 
     return <Grid item xs={12}>
-        <TableServerSide activityHistories={activityHistories} />
+        <TableServerSide activityHistories={data} />
     </Grid>
-
-
 }
 
 export default DataGrid
 
 
-export async function getServerSideProps() {
-    const data = await apiFetchAllActivityHistories();
+export async function getStaticProps() {
 
+    const data = await apiFetchAllActivityHistories()
 
     return {
         props: {
