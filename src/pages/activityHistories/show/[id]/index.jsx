2c4ea698d7be3a83961874d4c814c9729368e3bb
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-12 10:47:22 +0330

    static generation for details screen

--- src/pages/activityHistories/show/[id]/index.jsx
+++ src/pages/activityHistories/show/[id]/index.jsx
@@ -6,17 +6,41 @@ import ShowActivityHistory from '../components/ShowActivityHistory'
 import { useRouter } from 'next/router'
 import useActivityDetails from '../../hooks/useActivityDetails'
 import Spinner from 'src/components/Spinner'
+import { apiShowActivityHistory } from 'src/api/activityHistories/apiActivityHistories'
 
-const FormValidation = () => {
-    const router = useRouter()
-    const id = router.query.id
-    const { isLoading, activityHistory } = useActivityDetails(id)
+const FormValidation = ({ activityHistory }) => {
+    //     const router = useRouter()
+    //     const id = router.query.id
+    //     const { isLoading, activityHistory } = useActivityDetails(id)
+
+    //     return (
+    //         <Grid item xs={12}>
+    //             {isLoading ? <Spinner /> : <ShowActivityHistory activityHistory={activityHistory} />}
+    //         </Grid>
+    //     )
 
     return (
         <Grid item xs={12}>
-            {isLoading ? <Spinner /> : <ShowActivityHistory activityHistory={activityHistory} />}
+            <ShowActivityHistory activityHistory={activityHistory} />
         </Grid>
     )
 }
 
-export default FormValidation
+export default FormValidation;
+
+
+export async function getServerSideProps(context) {
+    //extract activity id 
+    const { params } = context;
+    const activityId = params.id;
+
+    // get activity details
+    const data = await apiShowActivityHistory(activityId);
+
+    return {
+        props: {
+            activityHistory: data
+        }
+    }
+
+}
