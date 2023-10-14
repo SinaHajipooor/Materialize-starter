be80831b060c8697c9d738217431fbeeec3bb063
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-14 09:50:48 +0330

    add api token in user credentials

--- src/pages/api/auth/options.js
+++ src/pages/api/auth/options.js
@@ -1,3 +1,4 @@
+import axios from "axios";
 import CredentialsProvider from "next-auth/providers/credentials";
 import apiLogin from "src/api/auth/login";
 
@@ -9,6 +10,17 @@ export const options = {
             async authorize(credentials) {
                 const response = await apiLogin(credentials);
                 const userData = response.data.result.user;
+                const apiToken = response.data.result.token;
+
+                const nextResponse = await fetch('/api/auth/token', {
+                    method: 'POST',
+                    body: JSON.stringify({ token: apiToken }),
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+
+                console.log(nextResponse)
 
                 return { ...userData, role: 'manager' }
             },
@@ -16,6 +28,7 @@ export const options = {
     ],
     callbacks: {
         async jwt({ token, user }) {
+
             if (user) {
                 return {
                     ...token, ...user
