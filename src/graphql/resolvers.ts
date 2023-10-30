de33ac22273a863332ff620a680cf6ec9e3d3991
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-10-30 21:33:20 +0330

    add graph ql and apollo server configs

---
+++ src/graphql/resolvers.ts
@@ -0,0 +1,38 @@
+export const resolvers = {
+    Query: {
+        links: () => [
+            {
+                category: 'category',
+                description: 'description',
+                id: '1',
+                imageUrl: 'dsdbsadv',
+                url: 'dsahgdshag'
+            },
+            {
+                category: 'category',
+                description: 'description',
+                id: '2',
+                imageUrl: 'dsdbsadv',
+                url: 'dsahgdshag'
+            }, {
+                category: 'category',
+                description: 'description',
+                id: '3',
+                imageUrl: 'dsdbsadv',
+                url: 'dsahgdshag'
+            }, {
+                category: 'category',
+                description: 'description',
+                id: '4',
+                imageUrl: 'dsdbsadv',
+                url: 'dsahgdshag'
+            }, {
+                category: 'category',
+                description: 'description',
+                id: '5',
+                imageUrl: 'dsdbsadv',
+                url: 'dsahgdshag'
+            }
+        ]
+    }
+}
