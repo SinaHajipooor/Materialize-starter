8290ed9f7c0ebd1da2fd9b7b0fbdd22a8bb1b84c
Sina Hajipour <dev.sinahajipour@gmail.com>
2023-11-11 14:09:00 +0330

    add graphql setup

--- src/pages/api/graphql.js
+++ src/pages/api/graphql.js
@@ -1,18 +1,14 @@
-import { ApolloServer } from "apollo-server-micro";
-import { typeDefs } from "../../schemas/schemas";
-import { resolvers } from "../../resolvers/resolvers";
+import { ApolloServer } from "@apollo/server";
+import { startServerAndCreateNextHandler } from '@as-integrations/next';
+import { resolvers } from "src/graphql/resolvers";
+import { typeDefs } from "src/graphql/schema";
+
 
 const apolloServer = new ApolloServer({
     typeDefs,
     resolvers,
 });
 
-export const config = {
-    api: {
-        bodyParser: false,
-    },
-};
-
-await apolloServer.start();
-
-export default apolloServer.createHandler({ path: "/api/graphql" });
+export default startServerAndCreateNextHandler(apolloServer, {
+    context: async (req, res) => ({ req, res })
+});
